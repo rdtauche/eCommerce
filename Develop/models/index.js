d35b0297184c7a687688f38873169ba69b3e7235
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-04-22 11:11:24 -0400

    finished up columns

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,14 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// RDT: Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// RDT: Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// RDT: Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+// RDT: Tags belongToMany Products (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
 
+
+  // RDT: module export
 module.exports = {
   Product,
   Category,
